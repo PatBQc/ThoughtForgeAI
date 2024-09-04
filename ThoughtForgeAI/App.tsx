4b301b8548d3c1b8f70d35fb470d89320e14147c
Patrick Bélanger <pbelanger@hotmail.com>
2024-09-04 13:40:26 -0400

    Finally a proper working splash screen

--- ThoughtForgeAI/App.tsx
+++ ThoughtForgeAI/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 import BrainstormScreen from './screens/BrainstormScreen';
 import ConversationFilesScreen from './screens/ConversationFilesScreen';
 import SettingsScreen from './screens/SettingsScreen';
@@ -10,6 +11,11 @@ import TabIcon from './TabIcon';
 const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
+  
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
--- ThoughtForgeAI/android/app/src/main/java/com/thoughtforgeai/MainActivity.kt
+++ ThoughtForgeAI/android/app/src/main/java/com/thoughtforgeai/MainActivity.kt
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen
 
 import android.os.Bundle;
 
@@ -23,6 +24,7 @@ class MainActivity : ReactActivity() {
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
   override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this)
         super.onCreate(null)
     }
 }
